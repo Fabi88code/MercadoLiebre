20fe1c9376b18a2c6d21aa2d8105b6926a3dabfb
Fabi88code <85083301+Fabi88code@users.noreply.github.com>
2021-07-08 20:46:15 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -19,9 +19,5 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(express.static(path.join(__dirname, './views')));
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log ("Servidor corriendo en el puerto 3000");
-  }
-  
-
-
-
+    console.log("Servidor corriendo en el puerto 3000");
+});
